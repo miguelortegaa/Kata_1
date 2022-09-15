9b13b95f2f37d555411e2c9ca82286e2fdbbaf63
Miguel Ortega <miguelorab@gmail.com>
2022-09-15 10:07:47 +0100

    Segunda versión de Kata_1

--- src/main/java/com/mycompany/kata_1/Kata_1.java
+++ src/main/java/com/mycompany/kata_1/Kata_1.java
@@ -1,11 +1,16 @@
 package com.mycompany.kata_1;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Kata_1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Juan", new Date(102, 10, 25));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1949, 9, 24);
+        
+        Person person = new Person("Juan", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
 }
